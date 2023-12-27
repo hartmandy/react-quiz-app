98071c00e9c2fe0280511af71acaf2c6a2503366
hartmandy <120344775+hartmandy@users.noreply.github.com>
2023-12-27 10:50:11 -0500

    initial commit

---
+++ src/components/Quiz.jsx
@@ -0,0 +1,46 @@
+import { useState } from "react";
+import quizCompletingImg from "../assets/quiz-complete.png";
+
+import QUESTIONS from "../questions";
+
+export default function Quiz() {
+  const [userAnswers, setUserAnswers] = useState([]);
+
+  const activeQuestionIndex = userAnswers.length;
+  const quizIsComplete = activeQuestionIndex === QUESTIONS.length;
+
+  function handleSelectAnswer(selectedAnswer) {
+    setUserAnswers((prevUserAnswers) => {
+      return [...prevUserAnswers, selectedAnswer];
+    });
+  }
+
+  if (quizIsComplete) {
+    return (
+      <div id="summary">
+        <img src={quizCompletingImg} alt="Trophy icon" />
+        <h2>Quiz Complete!</h2>
+      </div>
+    );
+  }
+
+  const shuffledAnswers = [...QUESTIONS[activeQuestionIndex].answers];
+  shuffledAnswers.sort(() => Math.random() - 0.5);
+
+  return (
+    <div id="quiz">
+      <div id="question">
+        <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
+        <ul id="answers">
+          {shuffledAnswers.map((answer) => (
+            <li key={answer} className="answer">
+              <button onClick={() => handleSelectAnswer(answer)}>
+                {answer}
+              </button>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+}
